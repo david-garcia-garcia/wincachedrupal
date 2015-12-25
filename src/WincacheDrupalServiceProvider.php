150d42fc06f2dc177fe0b20c70b32c5f0c2b64dc
david garcia <deivid.garcia.garcia@gmail.com>
2015-12-25 17:11:01 -0600

    Fixes.

--- src/WincacheDrupalServiceProvider.php
+++ src/WincacheDrupalServiceProvider.php
@@ -11,30 +11,50 @@ class WincachedrupalServiceProvider implements ServiceProviderInterface {
    * {@inheritdoc}
    */
   public function register(ContainerBuilder $container) {
+
     #region Tell Chained fast backend to use Wincache as default
-    $definition  = $container->getDefinition('cache.backend.chainedfast');
-    $args = $definition->getArguments();
-    // Add the missing optional arguments
-    if (!isset($args[1])) {
-      $args[] = NULL;
+    if ($container->has('cache.backend.chainedfast')) {
+      $definition  = $container->getDefinition('cache.backend.chainedfast');
+      $args = $definition->getArguments();
+      // Add the missing optional arguments
+      if (!isset($args[1])) {
+        $args[] = NULL;
+      }
+      if (!isset($args[2])) {
+        $args[] = 'cache.backend.wincache';
+      }
+      $definition->setArguments($args);
     }
-    if (!isset($args[2])) {
-      $args[] = 'cache.backend.wincache';
+    #endregion
+
+    #region Tell Chained fast backend to use Wincache as default
+    if ($container->has('cache.backend.superchainedfast')) {
+      $definition  = $container->getDefinition('cache.backend.superchainedfast');
+      $args = $definition->getArguments();
+      // Add the missing optional arguments
+      if (!isset($args[1])) {
+        $args[] = NULL;
+      }
+      if (!isset($args[2])) {
+        $args[] = 'cache.backend.wincache';
+      }
+      $definition->setArguments($args);
     }
-    $definition->setArguments($args);
     #endregion
 
     #region Tell Raw Chained fast backend to use Wincache as default
-    $definition  = $container->getDefinition('cache.rawbackend.chainedfast');
-    $args = $definition->getArguments();
-    // Add the missing optional arguments
-    if (!isset($args[1])) {
-      $args[] = NULL;
+    if ($container->has('cache.rawbackend.chainedfast')) {
+      $definition  = $container->getDefinition('cache.rawbackend.chainedfast');
+      $args = $definition->getArguments();
+      // Add the missing optional arguments
+      if (!isset($args[1])) {
+        $args[] = NULL;
+      }
+      if (!isset($args[2])) {
+        $args[] = 'cache.rawbackend.wincache';
+      }
+      $definition->setArguments($args);
     }
-    if (!isset($args[2])) {
-      $args[] = 'cache.rawbackend.wincache';
-    }
-    $definition->setArguments($args);
     #endregion
   }
 }
